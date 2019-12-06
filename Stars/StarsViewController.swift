b275efbf638a4972ed0a53f84de3bc5ea47e2d9c
morgan smith <mssmitty07@gmail.com>
2019-12-05 22:09:06 -0500

    added outlets

---
+++ Stars/StarsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Stars
+//
+//  Created by Morgan Smith on 12/5/19.
+//  Copyright © 2019 Morgan Smith. All rights reserved.
+//
+
+import UIKit
+
+class StarsViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var distanceTextField: UITextField!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func printStars(_ sender: Any) {
+    }
+    
+    @IBAction func createStar(_ sender: Any) {
+    }
+    
+
+
+}
+
